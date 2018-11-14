d35e9d4e883e0ec0601ee020a9d16965a2a60ab7
vladus2000 <docker@matt.land>
2018-11-14 12:27:38 -0600

    adding startup options

--- shiz/startup.sh
+++ shiz/startup.sh
@@ -1,5 +1,11 @@
 /fixuser.sh evil
 /unevil.sh
+if [[ ! -z "${PURGE}" ]]; then
+        /runevilorroot.sh SpiderOakONE --purge-historical-versions $PURGE
+fi
+if [[ ! -z "${DELETE}" ]]; then
+        /runevilorroot.sh SpiderOakONE --purge-deleted-items=$DELETE
+fi
 if [[ -x /customstartup.sh ]]; then
 	/runit.sh
 	/runevilorroot.sh /customstartup.sh
